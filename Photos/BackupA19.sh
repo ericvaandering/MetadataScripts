66b2d55f83649bb094ea0143fed8ff8433de52a4
Eric Vaandering <ericvaandering@gmail.com>
2019-06-12 22:03:44 -0500

    Add script for backing up Africa 2019 stuff

---
+++ Photos/BackupA19.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+rsync -a -v --delete /Volumes/Blue/A19-Photos/ /Volumes/NEXTODI/A19-Photos/
+
+rsync -a -v --delete /Users/ewv/data/A19-Lr /Volumes/Blue/A19-Lr 
+rsync -a -v --delete /Users/ewv/data/A19-Others /Volumes/Blue/A19-Others 
